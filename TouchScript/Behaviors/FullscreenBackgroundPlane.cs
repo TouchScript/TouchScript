e1375a70225e8e71cbb8cfa367a5fcf5e5a48050
Valentin Simonov <v@lent.in>
2012-10-27 17:06:06 +0400

    Fullscreen Background Plane

---
+++ TouchScript/Behaviors/FullscreenBackgroundPlane.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace TouchScript.Behaviors {
+    [AddComponentMenu("TouchScript/Fullscreen Background Target")]
+    [ExecuteInEditMode]
+    [RequireComponent(typeof (BoxCollider))]
+    [RequireComponent(typeof (Camera))]
+    public class FullscreenBackgroundTarget : MonoBehaviour {
+        protected void Update() {
+            var box = GetComponent<BoxCollider>();
+
+            var h = 2*camera.farClipPlane*Mathf.Tan(camera.fieldOfView/360*Mathf.PI);
+            var w = (float) Screen.width/Screen.height*h;
+
+            box.center = new Vector3(0, 0, camera.farClipPlane);
+            box.size = new Vector3(w, h, .1f);
+        }
+    }
+}
