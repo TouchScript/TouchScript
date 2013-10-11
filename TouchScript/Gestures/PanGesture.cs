ac8f69d93f2ea7a8df90c67fdabf61d03c47945d
Valentin Simonov <v@lent.in>
2013-10-11 18:37:48 +0400

    PanGesture now has its center point between two clusters if there are more than one point.

--- TouchScript/Gestures/PanGesture.cs
+++ TouchScript/Gestures/PanGesture.cs
@@ -2,6 +2,7 @@
  * @author Valentin Simonov / http://va.lent.in/
  */
 
+using System.Collections.Generic;
 using TouchScript.Clusters;
 using TouchScript.Gestures.Simple;
 using UnityEngine;
@@ -15,13 +16,16 @@ namespace TouchScript.Gestures
     public class PanGesture : SimplePanGesture
     {
 
+        private Clusters2 clusters = new Clusters2();
+
         /// <inheritdoc />
         public override Vector2 ScreenPosition
         {
             get
             {
                 if (activeTouches.Count == 0) return TouchPoint.InvalidPosition;
-                return Cluster.Get2DCenterPosition(activeTouches);
+                if (activeTouches.Count == 1) return activeTouches[0].Position;
+                return (clusters.GetCenterPosition(Clusters2.CLUSTER1) + clusters.GetCenterPosition(Clusters2.CLUSTER2)) * .5f;
             }
         }
 
@@ -31,8 +35,49 @@ namespace TouchScript.Gestures
             get
             {
                 if (activeTouches.Count == 0) return TouchPoint.InvalidPosition;
-                return Cluster.GetPrevious2DCenterPosition(activeTouches);
+                if (activeTouches.Count == 1) return activeTouches[0].PreviousPosition;
+                return (clusters.GetPreviousCenterPosition(Clusters2.CLUSTER1) + clusters.GetPreviousCenterPosition(Clusters2.CLUSTER2)) * .5f;
             }
+        }
+
+        /// <inheritdoc />
+        protected override void Awake()
+        {
+            base.Awake();
+
+            clusters.MinPointsDistance = 0;
+        }
+
+        /// <inheritdoc />
+        protected override void touchesBegan(IList<TouchPoint> touches)
+        {
+            clusters.AddPoints(touches);
+
+            base.touchesBegan(touches);
+        }
+
+        /// <inheritdoc />
+        protected override void touchesMoved(IList<TouchPoint> touches)
+        {
+            clusters.Invalidate();
+
+            base.touchesMoved(touches);
+        }
+
+        /// <inheritdoc />
+        protected override void touchesEnded(IList<TouchPoint> touches)
+        {
+            clusters.RemovePoints(touches);
+
+            base.touchesEnded(touches);
+        }
+
+        /// <inheritdoc />
+        protected override void reset()
+        {
+            base.reset();
+
+            clusters.RemoveAllPoints();
         }
 
     }
