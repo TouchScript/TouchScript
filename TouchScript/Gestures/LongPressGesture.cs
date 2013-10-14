7f3d21e4f3aae8ee55568a36eaabfd41ae7a3a6a
Valentin Simonov <v@lent.in>
2013-10-14 18:51:35 +0400

    Deleted SimpleLongPressGesture since it was the same as LongPressGesture.

--- TouchScript/Gestures/LongPressGesture.cs
+++ TouchScript/Gestures/LongPressGesture.cs
@@ -2,7 +2,8 @@
  * @author Valentin Simonov / http://va.lent.in/
  */
 
-using TouchScript.Clusters;
+using System.Collections;
+using System.Collections.Generic;
 using TouchScript.Gestures.Simple;
 using UnityEngine;
 
@@ -12,28 +13,152 @@ namespace TouchScript.Gestures
     /// Gesture which recognizes a point cluster which didn't move for specified time since it appeared.
     /// </summary>
     [AddComponentMenu("TouchScript/Gestures/Long Press Gesture")]
-    public class LongPressGesture : SimpleLongPressGesture
+    public class LongPressGesture : Gesture
     {
 
-        /// <inheritdoc />
-        public override Vector2 ScreenPosition
+        #region Private fields
+
+        [SerializeField]
+        private int maxTouches = int.MaxValue;
+
+        [SerializeField]
+        private float timeToPress = 1;
+
+        [SerializeField]
+        private float distanceLimit = float.PositiveInfinity;
+
+        private Vector2 totalMovement;
+        private float recognizeTime;
+        private bool fireRecognizedNextUpdate = false;
+
+        #endregion
+
+        #region Public properties
+
+        /// <summary>
+        /// Maximum number of simultaneous touch points.
+        /// </summary>
+        public int MaxTouches
         {
-            get
+            get { return maxTouches; }
+            set { maxTouches = value; }
+        }
+
+        /// <summary>
+        /// Total time in seconds required to hold touches still.
+        /// </summary>
+        public float TimeToPress
+        {
+            get { return timeToPress; }
+            set { timeToPress = value; }
+        }
+
+        /// <summary>
+        /// Maximum distance in cm touch points can move before gesture fails.
+        /// </summary>
+        public float DistanceLimit
+        {
+            get { return distanceLimit; }
+            set { distanceLimit = value; }
+        }
+
+        #endregion
+
+        #region Unity
+
+        /// <inheritdoc />
+        protected void Update()
+        {
+            if (fireRecognizedNextUpdate)
             {
-                if (activeTouches.Count == 0) return TouchPoint.InvalidPosition;
-                return Cluster.Get2DCenterPosition(activeTouches);
+                RaycastHit hit;
+                if (base.GetTargetHitResult(out hit))
+                {
+                    setState(GestureState.Recognized);
+                }
+                else
+                {
+                    setState(GestureState.Failed);
+                }
+            }
+        }
+
+        #endregion
+
+        #region Gesture callbacks
+
+        /// <inheritdoc />
+        protected override void touchesBegan(IList<TouchPoint> touches)
+        {
+            base.touchesBegan(touches);
+
+            if (activeTouches.Count > MaxTouches)
+            {
+                setState(GestureState.Failed);
+                return;
+            }
+            if (activeTouches.Count == touches.Count)
+            {
+                StartCoroutine("wait");
             }
         }
 
         /// <inheritdoc />
-        public override Vector2 PreviousScreenPosition
+        protected override void touchesMoved(IList<TouchPoint> touches)
         {
-            get
+            base.touchesMoved(touches);
+
+            totalMovement += ScreenPosition - PreviousScreenPosition;
+            if (totalMovement.magnitude / TouchManager.Instance.DotsPerCentimeter >= DistanceLimit)
             {
-                if (activeTouches.Count == 0) return TouchPoint.InvalidPosition;
-                return Cluster.GetPrevious2DCenterPosition(activeTouches);
+                setState(GestureState.Failed);
             }
         }
+
+        /// <inheritdoc />
+        protected override void touchesEnded(IList<TouchPoint> touches)
+        {
+            base.touchesEnded(touches);
+
+            if (activeTouches.Count == 0)
+            {
+                StopCoroutine("wait");
+                setState(GestureState.Failed);
+            }
+        }
+
+        /// <inheritdoc />
+        protected override void onFailed()
+        {
+            base.onFailed();
+
+            reset();
+        }
+
+        /// <inheritdoc />
+        protected override void reset()
+        {
+            base.reset();
+
+            fireRecognizedNextUpdate = false;
+            StopCoroutine("wait");
+        }
+
+        #endregion
+
+        #region Private functions
+
+        private IEnumerator wait()
+        {
+            recognizeTime = Time.time + TimeToPress;
+            while (Time.time < recognizeTime)
+            {
+                yield return null;
+            }
+            fireRecognizedNextUpdate = true;
+        }
+
+        #endregion
 
     }
 }
