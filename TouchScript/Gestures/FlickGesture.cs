9ef8fe880d407d499713bfd7a3dabde374cf10b2
Valentin Simonov <v@lent.in>
2013-10-14 18:53:45 +0400

    Removed SimpleFlickGesture since it was the same as FlickGesture.

--- TouchScript/Gestures/FlickGesture.cs
+++ TouchScript/Gestures/FlickGesture.cs
@@ -2,8 +2,7 @@
  * @author Valentin Simonov / http://va.lent.in/
  */
 
-using TouchScript.Clusters;
-using TouchScript.Gestures.Simple;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace TouchScript.Gestures
@@ -14,28 +13,209 @@ namespace TouchScript.Gestures
     /// Doesn't care how much time touch points were on surface and how much they moved.
     /// </summary>
     [AddComponentMenu("TouchScript/Gestures/Flick Gesture")]
-    public class FlickGesture : SimpleFlickGesture
+    public class FlickGesture : Gesture
     {
 
-        /// <inheritdoc />
-        public override Vector2 ScreenPosition
+        #region Unity fields
+
+        /// <summary>
+        /// Direction of a flick.
+        /// </summary>
+        public enum GestureDirection
         {
-            get
+            /// <summary>
+            /// Direction doesn't matter.
+            /// </summary>
+            Any,
+
+            /// <summary>
+            /// Only horizontal.
+            /// </summary>
+            Horizontal,
+
+            /// <summary>
+            /// Only vertical.
+            /// </summary>
+            Vertical,
+        }
+
+        #endregion
+
+        #region Private variables
+
+        [SerializeField]
+        private float flickTime = .5f;
+
+        [SerializeField]
+        private float minDistance = 1f;
+
+        [SerializeField]
+        private float movementThreshold = .5f;
+
+        [SerializeField]
+        private GestureDirection direction = GestureDirection.Any;
+
+        private bool moving = false;
+        private Vector2 movementBuffer = Vector2.zero;
+        private List<Vector2> positionDeltas = new List<Vector2>();
+        private List<float> timeDeltas = new List<float>();
+        private float previousTime;
+
+        #endregion
+
+        #region Public properties
+
+        /// <summary>
+        /// Time interval in seconds in which touch points must move by <see cref="MinDistance"/>.
+        /// </summary>
+        public float FlickTime
+        {
+            get { return flickTime; }
+            set { flickTime = value; }
+        }
+
+        /// <summary>
+        /// Minimum distance in cm to move in <see cref="FlickTime"/> before ending gesture for it to be recognized.
+        /// </summary>
+        public float MinDistance
+        {
+            get { return minDistance; }
+            set { minDistance = value; }
+        }
+
+        /// <summary>
+        /// Minimum distance in cm for touch points to move to be considered as a possible gesture. 
+        /// Prevents misinterpreting taps.
+        /// </summary>
+        public float MovementThreshold
+        {
+            get { return movementThreshold; }
+            set { movementThreshold = value; }
+        }
+
+        /// <summary>
+        /// Minimum distance in cm for touch points to move to be considered as a possible gesture. 
+        /// Prevents misinterpreting taps.
+        /// </summary>
+        public GestureDirection Direction
+        {
+            get { return direction; }
+            set { direction = value; }
+        }
+
+        /// <summary>
+        /// Contains flick direction (not normalized) when gesture is recognized.
+        /// </summary>
+        public Vector2 ScreenFlickVector { get; private set; }
+
+        #endregion
+
+        #region Gesture callbacks
+
+        /// <inheritdoc />
+        protected override void touchesBegan(IList<TouchPoint> touches)
+        {
+            base.touchesBegan(touches);
+
+            if (activeTouches.Count == touches.Count)
             {
-                if (activeTouches.Count == 0) return TouchPoint.InvalidPosition;
-                return Cluster.Get2DCenterPosition(activeTouches);
+                previousTime = Time.time;
             }
         }
 
         /// <inheritdoc />
-        public override Vector2 PreviousScreenPosition
+        protected override void touchesMoved(IList<TouchPoint> touches)
         {
-            get
+            base.touchesMoved(touches);
+
+            var delta = ScreenPosition - PreviousScreenPosition;
+            if (!moving)
             {
-                if (activeTouches.Count == 0) return TouchPoint.InvalidPosition;
-                return Cluster.GetPrevious2DCenterPosition(activeTouches);
+                movementBuffer += delta;
+                var dpiMovementThreshold = MovementThreshold * touchManager.DotsPerCentimeter;
+                if (movementBuffer.sqrMagnitude >= dpiMovementThreshold * dpiMovementThreshold)
+                {
+                    moving = true;
+                }
+            }
+
+            positionDeltas.Add(delta);
+            timeDeltas.Add(Time.time - previousTime);
+            previousTime = Time.time;
+        }
+
+        /// <inheritdoc />
+        protected override void touchesEnded(IList<TouchPoint> touches)
+        {
+            base.touchesEnded(touches);
+
+            if (activeTouches.Count == 0)
+            {
+                if (!moving)
+                {
+                    setState(GestureState.Failed);
+                    return;
+                }
+
+                var totalTime = 0f;
+                var totalMovement = Vector2.zero;
+                var i = timeDeltas.Count - 1;
+                while (i >= 0 && totalTime < FlickTime)
+                {
+                    if (totalTime + timeDeltas[i] < FlickTime)
+                    {
+                        totalTime += timeDeltas[i];
+                        totalMovement += positionDeltas[i];
+                        i--;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                switch (Direction)
+                {
+                    case GestureDirection.Horizontal:
+                        totalMovement.y = 0;
+                        break;
+                    case GestureDirection.Vertical:
+                        totalMovement.x = 0;
+                        break;
+                }
+
+                if (totalMovement.magnitude < MinDistance * TouchManager.Instance.DotsPerCentimeter)
+                {
+                    setState(GestureState.Failed);
+                }
+                else
+                {
+                    ScreenFlickVector = totalMovement;
+                    setState(GestureState.Recognized);
+                }
             }
         }
+
+        /// <inheritdoc />
+        protected override void touchesCancelled(IList<TouchPoint> touches)
+        {
+            base.touchesCancelled(touches);
+
+            touchesEnded(touches);
+        }
+
+        /// <inheritdoc />
+        protected override void reset()
+        {
+            base.reset();
+
+            moving = false;
+            movementBuffer = Vector2.zero;
+            timeDeltas.Clear();
+            positionDeltas.Clear();
+        }
+
+        #endregion
 
     }
 }
