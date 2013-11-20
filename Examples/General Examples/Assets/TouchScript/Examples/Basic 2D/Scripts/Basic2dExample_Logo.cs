c2da09df519d8311d50894b1d3386e35981303f3
Valentin Simonov <v@lent.in>
2013-11-20 20:36:14 +0400

    Assembling an example for 2D gestures.

---
+++ Examples/General Examples/Assets/TouchScript/Examples/Basic 2D/Scripts/Basic2dExample_Logo.cs
@@ -0,0 +1,61 @@
+﻿using TouchScript.Gestures;
+using UnityEngine;
+
+public class Basic2dExample_Logo : MonoBehaviour {
+
+    private enum State
+    {
+        Free,
+        Manual
+    }
+
+    private State state;
+    private Rigidbody2D rb;
+
+	void Start ()
+	{
+	    GetComponent<PressGesture>().StateChanged += (sender, args) =>
+	    {
+	        switch (args.State)
+	        {
+	            case Gesture.GestureState.Recognized:
+	                if (state == State.Free) stateManual();
+	                break;
+	        }
+	    };
+        GetComponent<ReleaseGesture>().StateChanged += (sender, args) =>
+        {
+            switch (args.State)
+            {
+                case Gesture.GestureState.Recognized:
+                    if (state == State.Manual) stateFree();
+                    break;
+            }
+        };
+
+	    rb = GetComponent<Rigidbody2D>();
+
+	    stateFree();
+	}
+	
+    private void stateFree()
+    {
+        setState(State.Free);
+
+        rb.gravityScale = 1;
+    }
+
+    private void stateManual()
+    {
+        setState(State.Manual);
+
+        rb.gravityScale = 0;
+    }
+
+    private void setState(State value)
+    {
+        state = value;
+        Debug.Log("State " + value);
+    }
+
+}
