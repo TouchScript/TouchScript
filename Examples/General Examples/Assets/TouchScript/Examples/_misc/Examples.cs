5b1ef0d4a33bd4a2246e19449b2160687f744384
Valentin Simonov <v@lent.in>
2014-02-07 01:35:58 +0400

    Examples scene for switching between examples. Demonstrates TouchScript working with scene reloading.

---
+++ Examples/General Examples/Assets/TouchScript/Examples/_misc/Examples.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class Examples : MonoBehaviour
+{
+
+    private static Examples instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        if (Application.loadedLevelName == "Examples") loadNextLevel();
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width - 120, 20, 100, 30), "Next >>"))
+        {
+            loadNextLevel();
+        }
+    }
+
+    private void loadNextLevel()
+    {
+        Application.LoadLevel((Application.loadedLevel + 1)%Application.levelCount);
+    }
+
+}
