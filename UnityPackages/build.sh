1a5ef343e027fd334cd8dc7391709929ebe97c5c
Valentin Simonov <v@lent.in>
2014-02-23 03:39:32 +0400

    Compile/copy scripts for packages.

--- UnityPackages/build.sh
+++ UnityPackages/build.sh
@@ -1,3 +1,11 @@
 #!bash
 
 /c/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe ../TouchScript.sln -p:Configuration=Release
+read -p "Press any key to continue... " -n 1 -s
+
+for i in $(ls -d */); do 
+	name=${i%%/}
+	if [ "$name" != "TouchScript" ]; then 
+		./sync_examples.sh "$name" 
+	fi
+done
---
+++ UnityPackages/sync_examples.sh
@@ -0,0 +1,25 @@
+#!bash
+
+if [[ $# -eq 0 ]] ; then
+    echo 'Error! Please supply project root folder.'
+    exit 0
+fi
+
+echo "Syncing TouchScript folder for $1."
+rm -rf tmp
+mkdir tmp
+rootfolder="$1/Assets/TouchScript/"
+IFS=$'\n'
+for f in $(git ls-tree -r --name-only HEAD | grep "^$rootfolder") ; do
+	echo "$f"
+	folder=$(dirname "$f")/
+	filename=$(basename "$f")
+	tmpfolder="tmp/${folder##$rootfolder}"
+	mkdir -p "$tmpfolder"
+	cp "$f" "$tmpfolder$filename"
+done
+
+rm -rf rootfolder
+cp -rf "TouchScript/Assets/TouchScript" "$1/Assets/"
+cp -r "tmp/." "$rootfolder"
+rm -rf tmp
