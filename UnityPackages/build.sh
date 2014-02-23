c7a6fb9c720327b04d6d103fd24193134ec02cbb
Valentin Simonov <v@lent.in>
2014-02-23 16:52:56 +0400

    Fixed build scripts and added colors.

--- UnityPackages/build.sh
+++ UnityPackages/build.sh
@@ -1,6 +1,6 @@
 #!bash
 
-printf "Synchronizing TouchScript folder...\n"
+printf "\e[32mSynchronizing TouchScript folder...\e[39m\n"
 for i in $(ls -d */); do 
 	name=${i%%/}
 	if [ "$name" != "TouchScript" ]; then 
@@ -8,5 +8,8 @@ for i in $(ls -d */); do
 	fi
 done
 
-printf "\nCompiling projects...\n"
+printf "\n\e[32mCompiling projects...\e[39m\n"
 /c/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe ../TouchScript.sln -p:Configuration=Release -clp:ErrorsOnly
+
+printf "\n\e[32mCompiling Windows Phone projects...\e[39m\n"
+/c/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe ../TouchScript.Other/TouchScript.WindowsPhone/TouchScript.WindowsPhone.sln -p:Configuration=Release -clp:ErrorsOnly
--- UnityPackages/sync_examples.sh
+++ UnityPackages/sync_examples.sh
@@ -1,25 +1,25 @@
 #!bash
 
 if [[ $# -eq 0 ]] ; then
-    echo 'Error! Please supply project root folder.'
+    echo '\e[31mError! Please supply project root folder.\e[39m'
     exit 0
 fi
 
-printf "\nSynchronizing TouchScript folder for $1.\n"
+printf "\n\e[32mSynchronizing TouchScript folder for $1.\e[39m\n"
 rm -rf tmp
 mkdir tmp
-rootfolder="$1/Assets/TouchScript/"
+rootfolder="$1/Assets/TouchScript"
 IFS=$'\n'
-for f in $(git ls-tree -r --name-only HEAD | grep "^$rootfolder") ; do
+for f in $(git ls-tree -r --name-only HEAD | grep "^$rootfolder/") ; do
 	echo "$f"
 	folder=$(dirname "$f")/
 	filename=$(basename "$f")
-	tmpfolder="tmp/${folder##$rootfolder}"
+	tmpfolder="tmp/${folder##$rootfolder/}"
 	mkdir -p "$tmpfolder"
 	cp "$f" "$tmpfolder$filename"
 done
 
-rm -rf rootfolder
+rm -rf $rootfolder
 cp -rf "TouchScript/Assets/TouchScript" "$1/Assets/"
-cp -r "tmp/." "$rootfolder"
+cp -r "tmp/." "$rootfolder/"
 rm -rf tmp
