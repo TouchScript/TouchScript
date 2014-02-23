03507c850e05e81cb130fad935e0f7a56ebd5082
Valentin Simonov <v@lent.in>
2014-02-23 22:28:39 +0400

    Windows Phone plugin to query dpi on device.

---
+++ TouchScript.Other/TouchScript.WindowsPhone/TouchScript.WindowsPhone/Devices/Display/DisplayDevice.cs
@@ -0,0 +1,34 @@
+﻿using TouchScript.Devices.Display;
+using UnityEngine;
+
+namespace TouchScript.WindowsPhone.Devices.Display
+{
+    public class DisplayDevice : ScriptableObject, IDisplayDevice
+    {
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public virtual float DPI
+        {
+            get { return dpi; }
+            set { dpi = value; }
+        }
+
+        [SerializeField]
+        protected new string name = "Unknown Device";
+
+        [SerializeField]
+        protected float dpi = 96;
+
+        protected virtual void OnEnable()
+        {
+            base.name = name;
+        }
+
+    }
+}
+
