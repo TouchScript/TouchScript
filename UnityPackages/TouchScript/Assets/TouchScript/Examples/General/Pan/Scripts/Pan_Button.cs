772581ce66d51ff8da59735695ee2bca41100a3f
Valentin Simonov <v@lent.in>
2014-03-10 23:04:16 +0400

    Pan and hierarchy example.

---
+++ UnityPackages/TouchScript/Assets/TouchScript/Examples/General/Pan/Scripts/Pan_Button.cs
@@ -0,0 +1,42 @@
+﻿using TouchScript.Gestures;
+using UnityEngine;
+
+public class Pan_Button : MonoBehaviour
+{
+
+    private float startY;
+
+    private void Awake()
+    {
+        startY = transform.localPosition.y;
+    }
+
+    private void OnEnable()
+    {
+        GetComponent<PressGesture>().StateChanged += pressStateChangeHandler;
+        GetComponent<ReleaseGesture>().StateChanged += releaseStateChangeHandler;
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<PressGesture>().StateChanged -= pressStateChangeHandler;
+        GetComponent<ReleaseGesture>().StateChanged -= releaseStateChangeHandler;
+    }
+
+    private void releaseStateChangeHandler(object sender, GestureStateChangeEventArgs e)
+    {
+        if (e.State == Gesture.GestureState.Recognized)
+        {
+            transform.localPosition = new Vector3(0, startY, 0);
+        }
+    }
+
+    private void pressStateChangeHandler(object sender, GestureStateChangeEventArgs e)
+    {
+        if (e.State == Gesture.GestureState.Recognized)
+        {
+            transform.localPosition = new Vector3(0, startY - transform.localScale.y * .9f, 0);
+        }
+    }
+
+}
---
+++ UnityPackages/TouchScript/Assets/TouchScript/Examples/General/Pan/Scripts/Pan_Object.cs
@@ -0,0 +1,37 @@
+﻿using TouchScript.Gestures;
+using UnityEngine;
+
+public class Pan_Object : MonoBehaviour
+{
+
+    private Quaternion targetRotation;
+
+	private void Awake()
+	{
+	    targetRotation = transform.localRotation;
+	}
+
+    private void OnEnable()
+    {
+        transform.FindChild("Button").GetComponent<TapGesture>().StateChanged += tapStateChangeHandler;
+    }
+
+    private void OnDisable()
+    {
+        transform.FindChild("Button").GetComponent<TapGesture>().StateChanged -= tapStateChangeHandler;
+    }
+	
+	void Update ()
+	{
+	    transform.localRotation = Quaternion.Lerp(transform.localRotation, targetRotation, .1f);
+	}
+
+    private void tapStateChangeHandler(object sender, GestureStateChangeEventArgs e)
+    {
+        if (e.State == Gesture.GestureState.Recognized)
+        {
+            targetRotation = Quaternion.Euler(0, 90, 0)*targetRotation;
+        }
+    }
+
+}
