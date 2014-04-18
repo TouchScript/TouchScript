dee1fcefffdd96abbaa23d8cea61b5f13e2bd628
Valentin Simonov <v@lent.in>
2014-04-18 13:28:50 +0200

    Input example.

---
+++ UnityPackages/TouchScript/Assets/TouchScript/Examples/General/Scripts/Examples.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Examples : MonoBehaviour
+{
+
+    private static Examples instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+
+        if (Application.loadedLevelName == "Examples" && Application.levelCount > 1)
+        {
+            loadNextLevel();
+            Destroy(GetComponent<Example>());
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width - 120, 20, 100, 30), "Next >>"))
+        {
+            loadNextLevel();
+        }
+    }
+
+    private void loadNextLevel()
+    {
+        Application.LoadLevel((Application.loadedLevel + 1)%Application.levelCount);
+    }
+
+}
