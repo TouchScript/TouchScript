82e7178d8aeed5904c47d6749bb8c5ae63a6bb56
Valentin Simonov <v@lent.in>
2014-05-02 17:35:04 +0400

    Removed WP8 plugin since 4.5 contains its functionality.

--- UnityPackages/build.sh
+++ UnityPackages/build.sh
@@ -8,6 +8,3 @@ done
 
 printf "\n\e[32mCompiling projects...\e[39m\n"
 /c/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe ../TouchScript.sln -p:Configuration=Release -clp:ErrorsOnly
-
-printf "\n\e[32mCompiling Windows Phone projects...\e[39m\n"
-/c/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe ../TouchScript.Other/TouchScript.WindowsPhone/TouchScript.WindowsPhone.sln -p:Configuration=Release -clp:ErrorsOnly
