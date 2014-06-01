94143668fcceabb0f187688536518daf23dd703e
Valentin Simonov <v@lent.in>
2014-06-01 21:54:04 +0400

    Layers example.

---
+++ UnityPackages/TouchScript/Assets/TouchScript/Examples/General/Layers/Scripts/Layers_Sprite.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using TouchScript.Gestures;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Layers_Sprite : MonoBehaviour {
+
+    private static Color[] COLORS = new[] { Color.yellow, Color.red, Color.magenta, Color.green, Color.cyan, Color.blue };
+
+    private void OnEnable()
+    {
+        GetComponent<TapGesture>().Tapped += tappedHandler;
+    }
+
+    private void OnDisable()
+    {
+        GetComponent<TapGesture>().Tapped -= tappedHandler;
+    }
+
+    private void changeColor()
+    {
+        Color newColor = COLORS[Random.Range(0, COLORS.Length)];
+        while (newColor == renderer.material.color) newColor = COLORS[Random.Range(0, COLORS.Length)];
+
+        renderer.material.color = newColor;
+    }
+
+    private void tappedHandler(object sender, EventArgs eventArgs)
+    {
+        changeColor();
+    }
+
+}
