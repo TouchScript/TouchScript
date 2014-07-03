93a7f2a3911b4d3becad580e4e0b23c4c20f1432
Valentin Simonov <v@lent.in>
2014-07-03 23:19:57 +0700

    Updated osx build scripts.

--- UnityPackages/build.sh
+++ UnityPackages/build.sh
@@ -7,4 +7,9 @@ for i in $(ls -d */); do
 done
 
 printf "\n\e[32mCompiling projects...\e[39m\n"
-/c/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe ../TouchScript.sln -p:Configuration=Release -clp:ErrorsOnly
+if [ "$(uname)" == "Darwin" ]; then
+	/Applications/Unity/MonoDevelop.app/Contents/MacOS/mdtool build --t:Clean --c:Release ../TouchScript.sln 
+    /Applications/Unity/MonoDevelop.app/Contents/MacOS/mdtool build --t:Build --c:Release ../TouchScript.sln  
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    /c/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe ../TouchScript.sln -p:Configuration=Release -clp:ErrorsOnly
+fi
