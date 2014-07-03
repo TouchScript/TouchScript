6eae40001562a3019b6a726ec94c1ce33e56f11f
Valentin Simonov <v@lent.in>
2014-07-03 18:26:51 +0700

    Extracted MouseInput functionality to MouseHandler internal class.

---
+++ TouchScript/InputSources/MouseHandler.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * @author Valentin Simonov / http://va.lent.in/
+ */
+
+using System;
+using UnityEngine;
+
+namespace TouchScript.InputSources
+{
+    internal class MouseHandler
+    {
+
+        private Func<Vector2, int> beginTouch;
+        private Action<int, Vector2> moveTouch;
+        private Action<int> endTouch;
+        private Action<int> cancelTouch;
+
+        private int mousePointId = -1;
+        private int fakeMousePointId = -1;
+        private Vector3 mousePointPos = Vector3.zero;
+
+        public MouseHandler(Func<Vector2, int> beginTouch, Action<int, Vector2> moveTouch, Action<int> endTouch,
+            Action<int> cancelTouch)
+        {
+            this.beginTouch = beginTouch;
+            this.moveTouch = moveTouch;
+            this.endTouch = endTouch;
+            this.cancelTouch = cancelTouch;
+
+            mousePointId = -1;
+            fakeMousePointId = -1;
+        }
+
+        public void Update()
+        {
+            var upHandled = false;
+            if (Input.GetMouseButtonUp(0))
+            {
+                if (mousePointId != -1)
+                {
+                    endTouch(mousePointId);
+                    mousePointId = -1;
+                    upHandled = true;
+                }
+            }
+
+            if (fakeMousePointId > -1 && !(Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)))
+            {
+                endTouch(fakeMousePointId);
+                fakeMousePointId = -1;
+            }
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                var pos = Input.mousePosition;
+                if ((Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)) && fakeMousePointId == -1)
+                {
+                    if (fakeMousePointId == -1) fakeMousePointId = beginTouch(new Vector2(pos.x, pos.y));
+                }
+                else
+                {
+                    if (mousePointId == -1) mousePointId = beginTouch(new Vector2(pos.x, pos.y));
+                }
+            }
+            else if (Input.GetMouseButton(0))
+            {
+                var pos = Input.mousePosition;
+                if (mousePointPos != pos)
+                {
+                    mousePointPos = pos;
+                    if (fakeMousePointId > -1 && mousePointId == -1)
+                    {
+                        moveTouch(fakeMousePointId, new Vector2(pos.x, pos.y));
+                    }
+                    else
+                    {
+                        moveTouch(mousePointId, new Vector2(pos.x, pos.y));
+                    }
+                }
+            }
+
+            if (Input.GetMouseButtonUp(0) && !upHandled)
+            {
+                endTouch(mousePointId);
+                mousePointId = -1;
+            }
+        }
+
+        public void Destroy()
+        {
+            if (mousePointId != -1) cancelTouch(mousePointId);
+            if (fakeMousePointId != -1) cancelTouch(fakeMousePointId);
+        }
+
+    }
+}
--- TouchScript/InputSources/MouseInput.cs
+++ TouchScript/InputSources/MouseInput.cs
@@ -13,6 +13,7 @@ namespace TouchScript.InputSources
     [AddComponentMenu("TouchScript/Input Sources/Mouse Input")]
     public sealed class MouseInput : InputSource
     {
+
         #region Public properties
 
         /// <summary>
@@ -33,9 +34,7 @@ namespace TouchScript.InputSources
 
         #region Private variables
 
-        private int mousePointId = -1;
-        private int fakeMousePointId = -1;
-        private Vector3 mousePointPos = Vector3.zero;
+        private MouseHandler mouseHandler;
 
         #endregion
 
@@ -44,6 +43,8 @@ namespace TouchScript.InputSources
         /// <inheritdoc />
         protected override void OnEnable()
         {
+            base.OnEnable();
+
             if (DisableOnMobilePlatforms)
             {
                 switch (Application.platform)
@@ -60,17 +61,14 @@ namespace TouchScript.InputSources
                 }
             }
 
-            base.OnEnable();
-
-            mousePointId = -1;
-            fakeMousePointId = -1;
+            mouseHandler = new MouseHandler((p) => beginTouch(p, new Tags(Tags)), moveTouch, endTouch, cancelTouch);
         }
 
         /// <inheritdoc />
         protected override void OnDisable()
         {
-            if (mousePointId != -1) cancelTouch(mousePointId);
-            if (fakeMousePointId != -1) cancelTouch(fakeMousePointId);
+            mouseHandler.Destroy();
+            mouseHandler = null;
 
             base.OnDisable();
         }
@@ -80,69 +78,9 @@ namespace TouchScript.InputSources
         {
             base.Update();
 
-            var upHandled = false;
-            if (Input.GetMouseButtonUp(0))
-            {
-                if (mousePointId != -1)
-                {
-                    endTouch(mousePointId);
-                    mousePointId = -1;
-                    upHandled = true;
-                }
-            }
-
-            if (fakeMousePointId > -1 && !(Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)))
-            {
-                endTouch(fakeMousePointId);
-                fakeMousePointId = -1;
-            }
-
-            if (Input.GetMouseButtonDown(0))
-            {
-                var pos = Input.mousePosition;
-                if ((Input.GetKey(KeyCode.LeftAlt) || Input.GetKey(KeyCode.RightAlt)) && fakeMousePointId == -1)
-                {
-                    if (fakeMousePointId == -1) fakeMousePointId = beginTouch(new Vector2(pos.x, pos.y)).Id;
-                }
-                else
-                {
-                    if (mousePointId == -1) mousePointId = beginTouch(new Vector2(pos.x, pos.y)).Id;
-                }
-            }
-            else if (Input.GetMouseButton(0))
-            {
-                var pos = Input.mousePosition;
-                if (mousePointPos != pos)
-                {
-                    mousePointPos = pos;
-                    if (fakeMousePointId > -1 && mousePointId == -1)
-                    {
-                        moveTouch(fakeMousePointId, new Vector2(pos.x, pos.y));
-                    }
-                    else
-                    {
-                        moveTouch(mousePointId, new Vector2(pos.x, pos.y));
-                    }
-                }
-            }
-
-            if (Input.GetMouseButtonUp(0) && !upHandled)
-            {
-                endTouch(mousePointId);
-                mousePointId = -1;
-            }
+            mouseHandler.Update();
         }
 
         #endregion
 
-        #region Protected methods
-
-        /// <inheritdoc />
-        protected override ITouch beginTouch(Vector2 position)
-        {
-            return beginTouch(position, new Tags(Tags));
-        }
-
-        #endregion
     }
-}
