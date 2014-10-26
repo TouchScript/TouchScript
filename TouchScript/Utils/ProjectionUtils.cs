7bad84f6c5d80fc3b3b5e934a5039f10ef253271
Valentin Simonov <valyard@valentin.local>
2014-10-26 21:35:05 +0300

    Refactored projection routines, made ProjectionUtils internal.

--- TouchScript/Utils/ProjectionUtils.cs
+++ TouchScript/Utils/ProjectionUtils.cs
@@ -10,9 +10,9 @@ namespace TouchScript.Utils
     /// <summary>
     /// Projection utils.
     /// </summary>
-    public static class ProjectionUtils
+    internal static class ProjectionUtils
     {
-        public static Vector3 NormalizeScreenPosition(Vector2 screenPosition)
+        internal static Vector3 NormalizeScreenPosition(Vector2 screenPosition)
         {
             return new Vector3(screenPosition.x/Screen.width - .5f, (screenPosition.y - Screen.height*.5f)/Screen.width, 0);
         }
@@ -25,29 +25,18 @@ namespace TouchScript.Utils
         /// <param name="planeOrigin">Projection plane origin.</param>
         /// <param name="planeNormal">Projection plane normal.</param>
         /// <returns>Projected point on the plane in World coordinates. If planeNormal is perpendicular to Camera.forward, returns Vector3.zero.</returns>
-        public static Vector3 CameraToPlaneProjection(Vector2 position, Camera camera, Vector3 planeOrigin, Vector3 planeNormal)
+        internal static Vector3 ProjectOnPlaneFromCamera(Vector2 position, Camera camera, Vector3 planeOrigin, Vector3 planeNormal)
         {
             var ray = camera.ScreenPointToRay(position);
-            var VdotN = Vector3.Dot(ray.direction, planeNormal);
-            if (Mathf.Approximately(VdotN, 0)) return Vector3.zero;
-
-            return ray.origin - (Vector3.Dot(ray.origin, planeNormal) - Vector3.Dot(planeOrigin, planeNormal)) / VdotN*ray.direction;
+            return ProjectOnPlane(ray.origin, ray.direction, planeOrigin, planeNormal);
         }
 
-        /// <summary>
-        /// Projects a screen point from the z=0 plane to another plane using parallel projection.
-        /// </summary>
-        /// <param name="position">Screen point.</param>
-        /// <param name="planeOrigin">Projection plane origin.</param>
-        /// <param name="planeNormal">Projection plane normal.</param>
-        /// <returns>Projected point on the plane in World coordinates. If planeNormal is perpendicular to Camera.forward, returns Vector3.zero.</returns>
-        public static Vector3 ScreenToPlaneProjection(Vector2 position, Vector3 planeOrigin, Vector3 planeNormal)
+        internal static Vector3 ProjectOnPlane(Vector3 point, Vector3 direction, Vector3 planeOrigin, Vector3 planeNormal)
         {
-            var ray = new Ray(position, Vector3.forward);
-            var VdotN = Vector3.Dot(ray.direction, planeNormal);
+            var VdotN = Vector3.Dot(direction, planeNormal);
             if (Mathf.Approximately(VdotN, 0)) return Vector3.zero;
 
-            return ray.origin - (Vector3.Dot(ray.origin, planeNormal) - Vector3.Dot(planeOrigin, planeNormal)) / VdotN * ray.direction;
+            return point - (Vector3.Dot(point, planeNormal) - Vector3.Dot(planeOrigin, planeNormal)) / VdotN * direction;
         }
     }
 }
