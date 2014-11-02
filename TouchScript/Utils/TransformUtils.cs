69ddcfdc0c014014e0f103b5fb1d44a6cf2d4a77
Valentin Simonov <valyard@valentin.local>
2014-11-02 20:11:20 +0300

    SimplePanGesture.LocalDeltaPosition fix for scaled objects.

--- TouchScript/Utils/TransformUtils.cs
+++ TouchScript/Utils/TransformUtils.cs
@@ -10,20 +10,29 @@ namespace TouchScript.Utils
     {
         public static Vector3 GlobalToLocalPosition(Transform transform, Vector3 global)
         {
-            if (transform.parent != null)
-            {
-                return transform.parent.InverseTransformPoint(global);
-            }
-            return global;
+            if (transform.parent == null) return global;
+            return transform.parent.InverseTransformPoint(global);
         }
 
         public static Vector3 GlobalToLocalDirection(Transform transform, Vector3 global)
         {
-            if (transform.parent != null)
-            {
-                return transform.parent.InverseTransformDirection(global);
-            }
-            return global;
+            if (transform.parent == null) return global;
+            return transform.parent.InverseTransformDirection(global);
+        }
+
+        public static Vector3 GlobalToLocalVector(Transform transform, Vector3 global)
+        {
+            var parent = transform.parent;
+            if (parent == null) return global;
+
+            var scale = parent.localScale;
+            var vector = GlobalToLocalVector(parent, global);
+            vector = Quaternion.Inverse(parent.localRotation) * vector;
+            vector.x = Mathf.Approximately(scale.x, 0) ? 0 : vector.x / scale.x;
+            vector.y = Mathf.Approximately(scale.y, 0) ? 0 : vector.y / scale.y;
+            vector.z = Mathf.Approximately(scale.z, 0) ? 0 : vector.z / scale.z;
+
+            return vector;
         }
     }
 }
