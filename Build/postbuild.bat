04bd3d3e9db9b91a759f78a3141c6ec0a48beed1
Valentin Simonov <v@lent.in>
2014-11-04 02:53:09 +0300

    Refactored postbuild actions.

---
+++ Build/postbuild.bat
@@ -0,0 +1,10 @@
+xcopy /y "%3" "%1UnityPackages\TouchScript\Assets\TouchScript\%2\"
+xcopy /y "%3" "%1UnityPackages\TouchScript.Android\Assets\TouchScript\%2\"
+xcopy /y "%3" "%1UnityPackages\TouchScript.iOS\Assets\TouchScript\%2\"
+xcopy /y "%3" "%1UnityPackages\TouchScript.TUIO\Assets\TouchScript\%2\"
+xcopy /y "%3" "%1UnityPackages\TouchScript.Windows7\Assets\TouchScript\%2\"
+xcopy /y "%3" "%1UnityPackages\TouchScript.WIndows8\Assets\TouchScript\%2\"
+xcopy /y "%3" "%1UnityPackages\TouchScript.WIndowsPhone\Assets\TouchScript\%2\"
+xcopy /y "%3" "%1UnityPackages\TouchScript.WindowsStore\Assets\TouchScript\%2\"
+
+if exist custom_postbuild.bat custom_postbuild.bat "%1" "%2" "%3"
