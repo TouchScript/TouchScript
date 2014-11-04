TouchScript/TouchScript

commit ac52e2a39e25f2a98484cb7fbcabe94076b4661b

Fixed an issue with axes-aligned limits on Transformer2D (issue #118).

TouchScript/Behaviors/Transformer2D.cs
@@ -57,22 +57,33 @@ namespace TouchScript.Behaviors
 
         private void Update()
         {
-            var fraction = Speed*Time.deltaTime;
-            if (transform.localPosition != lastLocalPosition)
-            {
-                // changed by someone else
-                localPositionToGo = transform.localPosition;
-            }
+            var fraction = Speed * Time.deltaTime;
+
+            // changed by someone else
+            if (!Mathf.Approximately(transform.localPosition.x, lastLocalPosition.x))
+                localPositionToGo.x = transform.localPosition.x;
+            if (!Mathf.Approximately(transform.localPosition.y, lastLocalPosition.y))
+                localPositionToGo.y = transform.localPosition.y;
+            if (!Mathf.Approximately(transform.localPosition.z, lastLocalPosition.z))
+                localPositionToGo.z = transform.localPosition.z;
             transform.localPosition = lastLocalPosition = Vector3.Lerp(transform.localPosition, localPositionToGo, fraction);
-            if (transform.localScale != lastLocalScale)
-            {
-                localScaleToGo = transform.localScale;
-            }
+
+            // changed by someone else
+            if (!Mathf.Approximately(transform.localScale.x, lastLocalScale.x))
+                localScaleToGo.x = transform.localScale.x;
+            if (!Mathf.Approximately(transform.localScale.y, lastLocalScale.y))
+                localScaleToGo.y = transform.localScale.y;
+            if (!Mathf.Approximately(transform.localScale.z, lastLocalScale.z))
+                localScaleToGo.z = transform.localScale.z;
             transform.localScale = lastLocalScale = Vector3.Lerp(transform.localScale, localScaleToGo, fraction);
-            if (transform.localRotation != lastLocalRotation)
-            {
-                localRotationToGo = transform.localRotation;
-            }
+
+            // changed by someone else
+            if (!Mathf.Approximately(transform.localRotation.x, lastLocalRotation.x))
+                localRotationToGo.x = transform.localRotation.x;
+            if (!Mathf.Approximately(transform.localRotation.y, lastLocalRotation.y))
+                localRotationToGo.y = transform.localRotation.y;
+            if (!Mathf.Approximately(transform.localRotation.z, lastLocalRotation.z))
+                localRotationToGo.z = transform.localRotation.z;
             transform.localRotation = lastLocalRotation = Quaternion.Lerp(transform.localRotation, localRotationToGo, fraction);
         }
 
@@ -107,7 +118,8 @@ namespace TouchScript.Behaviors
                 if (transform.parent == null)
                 {
                     localRotationToGo = Quaternion.AngleAxis(gesture.DeltaRotation, gesture.RotationAxis) * localRotationToGo;
-                } else
+                }
+                else
                 {
                     localRotationToGo = Quaternion.AngleAxis(gesture.DeltaRotation, transform.parent.InverseTransformDirection(gesture.RotationAxis)) * localRotationToGo;
                 }

commit 8a009b6095721b157c0d75c52bc9a8fafd4d0383

Fixed a regression with Transformer2D introduced in ac52e2a39e25f2a98484cb7fbcabe94076b4661b

TouchScript/Behaviors/Transformer2D.cs
@@ -78,12 +78,7 @@ namespace TouchScript.Behaviors
             transform.localScale = lastLocalScale = Vector3.Lerp(transform.localScale, localScaleToGo, fraction);
 
             // changed by someone else
-            if (!Mathf.Approximately(transform.localRotation.x, lastLocalRotation.x))
-                localRotationToGo.x = transform.localRotation.x;
-            if (!Mathf.Approximately(transform.localRotation.y, lastLocalRotation.y))
-                localRotationToGo.y = transform.localRotation.y;
-            if (!Mathf.Approximately(transform.localRotation.z, lastLocalRotation.z))
-                localRotationToGo.z = transform.localRotation.z;
+            if (transform.localRotation != lastLocalRotation) localRotationToGo = transform.localRotation;
             transform.localRotation = lastLocalRotation = Quaternion.Lerp(transform.localRotation, localRotationToGo, fraction);
         }
 