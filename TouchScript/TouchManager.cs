a8ebee4eff00b5d78c670d27b6906cb62cb9e600
Valentin Simonov <v@lent.in>
2015-03-06 18:39:25 +0300

    Uped version.

--- TouchScript/TouchManager.cs
+++ TouchScript/TouchManager.cs
@@ -125,7 +125,7 @@ namespace TouchScript
         /// <summary>
         /// TouchScript version.
         /// </summary>
-        public static readonly Version VERSION = new Version(6, 5);
+        public static readonly Version VERSION = new Version(6, 6);
 
         #endregion
 
