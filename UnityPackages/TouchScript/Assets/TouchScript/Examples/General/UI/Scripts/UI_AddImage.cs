a09101c1fc4026b7db384192c95cd13a1dbbac66
Valentin Simonov <v@lent.in>
2015-05-25 18:13:10 +0300

    UILayer example.

---
+++ UnityPackages/TouchScript/Assets/TouchScript/Examples/General/UI/Scripts/UI_AddImage.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UI_AddImage : MonoBehaviour 
+{
+
+	public void AddImage()
+	{
+		var toClone = transform.GetChild(Random.Range(0, transform.childCount));
+		var clone = Instantiate(toClone.gameObject) as GameObject;
+		clone.transform.SetParent(transform);
+		clone.transform.localScale = Vector3.one;
+		clone.transform.localRotation = Quaternion.Euler(0f, 0f, Random.Range(0f, 360f));
+		clone.transform.localPosition = new Vector3(Random.Range(-500, 500), Random.Range(-500, 500), toClone.localPosition.z);
+	}
+
+}
---
+++ UnityPackages/TouchScript/Assets/TouchScript/Examples/General/UI/Scripts/UI_SetColor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class UI_SetColor : MonoBehaviour 
+{
+
+	public List<Color> Colors;
+
+	public void SetColor(int id) 
+	{
+		GetComponent<Image>().color = Colors[id];
+	}
+
+}
