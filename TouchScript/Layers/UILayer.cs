4d0ee69e329d4c250d38e1dec309a69952696153
Valentin Simonov <v@lent.in>
2015-06-01 00:12:38 +0300

    UILayer tmp.

--- TouchScript/Layers/UILayer.cs
+++ TouchScript/Layers/UILayer.cs
@@ -57,40 +57,14 @@ namespace TouchScript.Layers
 
         [NonSerialized]
         private List<RaycastResult> raycastResultCache = new List<RaycastResult>();
-
         private PointerEventData pointerDataCache;
+        private EventSystem eventSystem;
+
+        protected Dictionary<int, PointerEventData> pointerEvents = new Dictionary<int, PointerEventData>();
 
         #endregion
 
         #region Public methods
-
-        /// <inheritdoc />
-        public override LayerHitResult Hit(Vector2 position, out TouchHit hit)
-        {
-            if (base.Hit(position, out hit) == LayerHitResult.Miss) return LayerHitResult.Miss;
-
-            hit = default(TouchHit);
-            var eventSystem = EventSystem.current;
-            if (eventSystem == null) return LayerHitResult.Error;
-
-            if (pointerDataCache == null) pointerDataCache = new PointerEventData(eventSystem);
-            pointerDataCache.position = position;
-            eventSystem.RaycastAll(pointerDataCache, raycastResultCache);
-
-            var raycast = findFirstRaycast(raycastResultCache);
-            raycastResultCache.Clear();
-            if (raycast.gameObject == null) return LayerHitResult.Miss;
-
-            if (!(raycast.module is GraphicRaycaster))
-            {
-                if (Application.isEditor)
-                    Debug.LogWarning("UILayer in Layer mode doesn't support raycasters other than GraphicRaycaster. Please use CameraLayer or CameraLayer2D to hit 3d objects.");
-                return LayerHitResult.Miss;
-            }
-
-            hit = new TouchHit(raycast);
-            return LayerHitResult.Hit;
-        }
 
         public override ProjectionParams GetProjectionParams(ITouch touch)
         {
@@ -141,6 +115,107 @@ namespace TouchScript.Layers
             Name = "UI Layer";
         }
 
+        protected override LayerHitResult beginTouch(ITouch touch, out TouchHit hit)
+        {
+            hit = default(TouchHit);
+            if (enabled == false || gameObject.activeInHierarchy == false) return LayerHitResult.Miss;
+            if (eventSystem == null) return LayerHitResult.Error;
+
+            if (pointerDataCache == null) pointerDataCache = new PointerEventData(eventSystem);
+            pointerDataCache.position = touch.Position;
+            eventSystem.RaycastAll(pointerDataCache, raycastResultCache);
+
+            var raycast = findFirstRaycast(raycastResultCache);
+            raycastResultCache.Clear();
+            if (raycast.gameObject == null) return LayerHitResult.Miss;
+
+            if (mode == UILayerMode.Layer)
+            {
+                if (!(raycast.module is GraphicRaycaster))
+                {
+                    if (Application.isEditor)
+                        Debug.LogWarning("UILayer in Layer mode doesn't support raycasters other than GraphicRaycaster. Please use CameraLayer or CameraLayer2D to hit 3d objects.");
+                    return LayerHitResult.Miss;
+                }
+
+                hit = new TouchHit(raycast);
+            }
+            else
+            {
+                // don't init hit, no target --> layer consumes touch
+
+                PointerEventData pointerEvent;
+                getPointerData(touch.Id, out pointerEvent, true);
+
+                pointerEvent.pointerPressRaycast = pointerEvent.pointerCurrentRaycast = raycast;
+                pointerEvent.position = touch.Position;
+                pointerEvent.button = PointerEventData.InputButton.Left;
+                pointerEvent.eligibleForClick = true;
+                pointerEvent.delta = Vector2.zero;
+                pointerEvent.dragging = false;
+                pointerEvent.useDragThreshold = true;
+                pointerEvent.pressPosition = pointerEvent.position;
+
+                var currentOverGo = pointerEvent.pointerCurrentRaycast.gameObject;
+
+                deselectIfSelectionChanged(currentOverGo, pointerEvent);
+
+                if (pointerEvent.pointerEnter != currentOverGo)
+                {
+                    // send a pointer enter to the touched element if it isn't the one to select...
+                    HandlePointerExitAndEnter(pointerEvent, currentOverGo);
+                    pointerEvent.pointerEnter = currentOverGo;
+                }
+
+                // search for the control that will receive the press
+                // if we can't find a press handler set the press
+                // handler to be what would receive a click.
+                var newPressed = ExecuteEvents.ExecuteHierarchy(currentOverGo, pointerEvent, ExecuteEvents.pointerDownHandler);
+
+                // didnt find a press handler... search for a click handler
+                if (newPressed == null)
+                    newPressed = ExecuteEvents.GetEventHandler<IPointerClickHandler>(currentOverGo);
+
+                // TODO: double-tap
+                pointerEvent.clickCount = 1;
+                pointerEvent.pointerPress = newPressed;
+                pointerEvent.rawPointerPress = currentOverGo;
+                pointerEvent.clickTime = Time.unscaledTime;
+
+                // Save the drag handler as well
+                pointerEvent.pointerDrag = ExecuteEvents.GetEventHandler<IDragHandler>(currentOverGo);
+
+                if (pointerEvent.pointerDrag != null)
+                    ExecuteEvents.Execute(pointerEvent.pointerDrag, pointerEvent, ExecuteEvents.initializePotentialDrag);
+            }
+
+            return LayerHitResult.Hit;
+        }
+
+        protected bool getPointerData(int id, out PointerEventData data, bool create)
+        {
+            if (!pointerEvents.TryGetValue(id, out data) && create)
+            {
+                data = new PointerEventData(eventSystem)
+                {
+                    pointerId = id,
+                };
+                pointerEvents.Add(id, data);
+                return true;
+            }
+            return false;
+        }
+
+        protected void deselectIfSelectionChanged(GameObject currentOverGo, BaseEventData pointerEvent)
+        {
+            // Selection tracking
+            var selectHandlerGO = ExecuteEvents.GetEventHandler<ISelectHandler>(currentOverGo);
+            // if we have clicked something new, deselect the old thing
+            // leave 'selection handling' up to the press event though.
+            if (selectHandlerGO != eventSystem.currentSelectedGameObject)
+                eventSystem.SetSelectedGameObject(null, pointerEvent);
+        }
+
         #endregion
 
         #region Private functions
@@ -159,6 +234,7 @@ namespace TouchScript.Layers
 
         private void activateMode()
         {
+            eventSystem = EventSystem.current;
             switch (mode)
             {
                 case UILayerMode.InputModule:
