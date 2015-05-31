46f5d81bcd65db6759e113a393257511ec358132
Valentin Simonov <v@lent.in>
2015-05-31 17:41:18 +0300

    Moved WindowsInput to main assembly, removed win7 and win8 projects.

--- Build/postbuild.sh
+++ Build/postbuild.sh
@@ -3,8 +3,6 @@ for file in "${@:3}" ; do
 	cp "$file" "$1/UnityPackages/TouchScript.Android/Assets/TouchScript/$2/"
 	cp "$file" "$1/UnityPackages/TouchScript.iOS/Assets/TouchScript/$2/"
 	cp "$file" "$1/UnityPackages/TouchScript.TUIO/Assets/TouchScript/$2/"
-	cp "$file" "$1/UnityPackages/TouchScript.Windows7/Assets/TouchScript/$2/"
-	cp "$file" "$1/UnityPackages/TouchScript.Windows8/Assets/TouchScript/$2/"
 	cp "$file" "$1/UnityPackages/TouchScript.WindowsPhone/Assets/TouchScript/$2/"
 	cp "$file" "$1/UnityPackages/TouchScript.WindowsStore/Assets/TouchScript/$2/"
 
