6526da20c308f72132e4a4ccbddac5db1d14a5f5
Valentin Simonov <v@lent.in>
2015-07-24 23:14:26 +0300

    Camera example scene.

---
+++ UnityPackages/TouchScript/Assets/TouchScript/Examples/General/Camera/Scripts/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+
+namespace TouchScript.Examples.Camera {
+
+	public class CameraController : MonoBehaviour {
+
+		public float PanSpeed = 200f;
+		public float ZoomSpeed = 1f;
+
+		private TransformGesture gesture;
+		private Transform camera;
+
+		private void Awake() {
+			camera = transform.FindChild("Camera");
+		}
+
+		private void OnEnable() {
+			gesture = GetComponent<TransformGesture>();
+			gesture.Transformed += transformedHandler;
+		}
+
+		private void OnDisable() {
+			gesture.Transformed -= transformedHandler;
+		}
+
+		private void transformedHandler (object sender, System.EventArgs e)
+		{
+			var rotation = Quaternion.Euler(gesture.DeltaPosition.y / Screen.height * PanSpeed, -gesture.DeltaPosition.x / Screen.width * PanSpeed, gesture.DeltaRotation);
+			transform.localRotation *= rotation;
+
+			camera.transform.localPosition += Vector3.forward * (gesture.DeltaScale - 1f) * ZoomSpeed;
+		}
+		
+	}
+
+}
