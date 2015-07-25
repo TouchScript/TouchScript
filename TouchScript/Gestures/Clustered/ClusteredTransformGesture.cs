e409ee420d160e3c3f33793e57a5fbecc9ae1656
Valentin Simonov <v@lent.in>
2015-07-26 01:19:29 +0300

    ClusteredTransformGesture.

--- TouchScript/Gestures/Clustered/ClusteredTransformGesture.cs
+++ TouchScript/Gestures/Clustered/ClusteredTransformGesture.cs
@@ -2,10 +2,8 @@
  * @author Valentin Simonov / http://va.lent.in/
  */
 
-using System;
 using System.Collections.Generic;
 using TouchScript.Utils;
-using TouchScript.Gestures.Simple;
 using UnityEngine;
 
 namespace TouchScript.Gestures.Clustered
@@ -14,5 +12,86 @@ namespace TouchScript.Gestures.Clustered
     [AddComponentMenu("TouchScript/Gestures/Clustered/Transform Gesture (Clustered)")]
     public class ClusteredTransformGesture : TransformGesture
     {
+
+        #region Private variables
+
+        private Clusters.Clusters clusters = new Clusters.Clusters();
+
+        #endregion
+
+        #region Gesture callbacks
+
+        /// <inheritdoc />
+        protected override void touchesBegan(IList<ITouch> touches)
+        {
+            clusters.AddPoints(touches);
+
+            base.touchesBegan(touches);
+        }
+
+        /// <inheritdoc />
+        protected override void touchesMoved(IList<ITouch> touches)
+        {
+            clusters.Invalidate();
+
+            base.touchesMoved(touches);
+        }
+
+        /// <inheritdoc />
+        protected override void touchesEnded(IList<ITouch> touches)
+        {
+            clusters.RemovePoints(touches);
+
+            base.touchesEnded(touches);
+        }
+
+        /// <inheritdoc />
+        protected override void reset()
+        {
+            base.reset();
+
+            clusters.RemoveAllPoints();
+        }
+
+        #endregion
+
+        #region Protected methods
+
+        /// <inheritdoc />
+        protected override int getNumPoints()
+        {
+            if (clusters.HasClusters) return 2;
+            if (NumTouches > 0) return 1;
+            return 0;
+        }
+
+        /// <inheritdoc />
+        protected override bool relevantTouches(IList<ITouch> touches)
+        {
+            return true;
+        }
+
+        /// <inheritdoc />
+        protected override Vector2 getPointScreenPosition(int index)
+        {
+            if (!clusters.HasClusters) return ClusterUtils.Get2DCenterPosition(activeTouches);
+
+            if (index < 0) index = 0;
+            else if (index > 1) index = 1;
+            return clusters.GetCenterPosition(index);
+        }
+
+        /// <inheritdoc />
+        protected override Vector2 getPointPreviousScreenPosition(int index)
+        {
+            if (!clusters.HasClusters) return ClusterUtils.GetPrevious2DCenterPosition(activeTouches);
+
+            if (index < 0) index = 0;
+            else if (index > 1) index = 1;
+            return clusters.GetPreviousCenterPosition(index);
+        }
+
+        #endregion
+
     }
 }
