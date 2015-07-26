cb3a9ff50af508b6d5e5a236e7fb51d5154613ff
Valentin Simonov <v@lent.in>
2015-07-26 19:33:09 +0300

    Changed .gitignore and checked in Utils/Debug folder.

---
+++ TouchScript/Utils/Debug/DebugHelper.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * @author Valentin Simonov / http://va.lent.in/
+ */
+
+using UnityEngine;
+
+#if DEBUG
+
+namespace TouchScript.Utils.Debug
+{
+    public static class DebugHelper
+    {
+        public static int GetDebugId(Object obj)
+        {
+            return (obj.GetInstanceID() >> 10) << 10;
+        }
+    }
+}
+
+#endif
