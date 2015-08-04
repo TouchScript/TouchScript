80311e3eef1dea93c1f3f864dbab72672b78c93f
Valentin Simonov <v@lent.in>
2015-08-04 03:48:29 +0300

    Checkers example.

---
+++ UnityPackages/TouchScript/Assets/TouchScript/Examples/General/Checkers/Scripts/Board.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using TouchScript.Gestures;
+
+namespace TouchScript.Examples.Checkers {
+
+	public class Board : MonoBehaviour {
+
+		PinnedTransformGesture gesture;
+
+		private void OnEnable() {
+			gesture = GetComponent<PinnedTransformGesture>();
+			gesture.TransformStarted += transformStartedHandler;
+			gesture.Transformed += transformedHandler;
+		}
+		
+		private void OnDisable() {
+			gesture.Transformed -= transformedHandler;
+		}
+
+		private void transformStartedHandler(object sender, System.EventArgs e)
+		{
+		}
+
+		private void transformedHandler(object sender, System.EventArgs e)
+		{
+			transform.localRotation *= Quaternion.AngleAxis(gesture.DeltaRotation, gesture.RotationAxis);
+		}
+		
+	}
+
+}
