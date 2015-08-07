0521c5b7de4097f672e9180ce3e464bb6b69e06b
Valentin Simonov <v@lent.in>
2015-08-07 20:05:52 +0300

    Updated examples.

--- UnityPackages/TouchScript/Assets/TouchScript/Examples/General/Camera/Scripts/CameraController.cs
+++ UnityPackages/TouchScript/Assets/TouchScript/Examples/General/Camera/Scripts/CameraController.cs
@@ -9,11 +9,13 @@ namespace TouchScript.Examples.Camera {
 		public float PanSpeed = 200f;
 		public float ZoomSpeed = 10f;
 
+		private Transform pivot;
 		private ScreenTransformGesture gesture;
 		private Transform cam;
 
 		private void Awake() {
-			cam = transform.FindChild("Camera");
+			pivot = transform.Find("Pivot");
+			cam = transform.Find("Pivot/Camera");
 		}
 
 		private void OnEnable() {
@@ -28,7 +30,7 @@ namespace TouchScript.Examples.Camera {
 		private void transformedHandler (object sender, System.EventArgs e)
 		{
 			var rotation = Quaternion.Euler(gesture.DeltaPosition.y / Screen.height * PanSpeed, -gesture.DeltaPosition.x / Screen.width * PanSpeed, gesture.DeltaRotation);
-			transform.localRotation *= rotation;
+			pivot.localRotation *= rotation;
 
 			cam.transform.localPosition += Vector3.forward * (gesture.DeltaScale - 1f) * ZoomSpeed;
 		}
