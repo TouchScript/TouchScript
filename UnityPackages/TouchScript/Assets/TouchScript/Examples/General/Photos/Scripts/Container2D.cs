1d0150d1332acac597553f0bdcd9e2640c01292b
Valentin Simonov <v@lent.in>
2015-10-04 01:18:52 +0300

    2D Photos example.

--- UnityPackages/TouchScript/Assets/TouchScript/Examples/General/Photos/Scripts/Container2D.cs
+++ UnityPackages/TouchScript/Assets/TouchScript/Examples/General/Photos/Scripts/Container2D.cs
@@ -3,37 +3,39 @@ using System.Collections;
 using System.Collections.Generic;
 using TouchScript.Gestures;
 
-namespace TouchScript.Examples.Photos {
-	public class Container : MonoBehaviour {
-
-		private List<Transform> children;
-
-		private void Start() {
+namespace TouchScript.Examples.Photos 
+{
+	public class Container2D : MonoBehaviour 
+	{
+		private List<SpriteRenderer> children;
+		
+		private void Start() 
+		{
 			var count = transform.childCount;
-			children = new List<Transform>(count);
+			children = new List<SpriteRenderer>(count);
 			for (var i = 0; i < count; i++)
 			{
-				var child = transform.GetChild(i);
+				var child = transform.GetChild(i).GetComponent<SpriteRenderer>();
 				children.Add(child);
 				var pressGesture = child.GetComponent<PressGesture>();
 				if (pressGesture != null) pressGesture.Pressed += pressedHandler;
 			}
 			sortChildren();
 		}
-
+		
 		private void sortChildren()
 		{
 			var count = children.Count;
 			for (var i = 0; i < count; i++)
 			{
 				var child = children[i];
-				child.localPosition = new Vector3(child.localPosition.x, i * 0.003f, child.localPosition.z);
+				child.sortingOrder = i;
 			}
 		}
-
+		
 		private void pressedHandler(object sender, System.EventArgs e)
 		{
-			var child = (sender as Gesture).transform;
+			var child = (sender as Gesture).GetComponent<SpriteRenderer>();
 			children.Remove(child);
 			children.Add(child);
 			sortChildren();
