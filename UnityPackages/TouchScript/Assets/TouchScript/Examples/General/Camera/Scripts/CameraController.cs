3e042625976984226b560517e0aa4891b2bdfac4
Valentin Simonov <v@lent.in>
2015-10-06 04:54:16 +0300

    Updated Camera example to use two gestures.

--- UnityPackages/TouchScript/Assets/TouchScript/Examples/General/Camera/Scripts/CameraController.cs
+++ UnityPackages/TouchScript/Assets/TouchScript/Examples/General/Camera/Scripts/CameraController.cs
@@ -8,33 +8,46 @@ namespace TouchScript.Examples.CameraControl
 	public class CameraController : MonoBehaviour 
 	{
 
+		public ScreenTransformGesture TwoFingerMoveGesture;
+		public ScreenTransformGesture ManipulationGesture;
 		public float PanSpeed = 200f;
+		public float RotationSpeed = 200f;
 		public float ZoomSpeed = 10f;
 
 		private Transform pivot;
-		private ScreenTransformGesture gesture;
 		private Transform cam;
 
-		private void Awake() {
+		private void Awake() 
+		{
 			pivot = transform.Find("Pivot");
 			cam = transform.Find("Pivot/Camera");
 		}
 
-		private void OnEnable() {
-			gesture = GetComponent<ScreenTransformGesture>();
-			gesture.Transformed += transformedHandler;
-		}
-
-		private void OnDisable() {
-			gesture.Transformed -= transformedHandler;
-		}
-
-		private void transformedHandler (object sender, System.EventArgs e)
+		private void OnEnable() 
 		{
-			var rotation = Quaternion.Euler(gesture.DeltaPosition.y / Screen.height * PanSpeed, -gesture.DeltaPosition.x / Screen.width * PanSpeed, gesture.DeltaRotation);
+			TwoFingerMoveGesture.Transformed += twoFingerTransformHandler;
+			ManipulationGesture.Transformed += manipulationTransformedHandler;
+		}
+
+		private void OnDisable() 
+		{
+			TwoFingerMoveGesture.Transformed -= twoFingerTransformHandler;
+			ManipulationGesture.Transformed -= manipulationTransformedHandler;
+		}
+
+		private void manipulationTransformedHandler(object sender, System.EventArgs e)
+		{
+			var rotation = Quaternion.Euler(ManipulationGesture.DeltaPosition.y / Screen.height * RotationSpeed, 
+			                                -ManipulationGesture.DeltaPosition.x / Screen.width * RotationSpeed, 
+			                                ManipulationGesture.DeltaRotation);
 			pivot.localRotation *= rotation;
 
-			cam.transform.localPosition += Vector3.forward * (gesture.DeltaScale - 1f) * ZoomSpeed;
+			cam.transform.localPosition += Vector3.forward * (ManipulationGesture.DeltaScale - 1f) * ZoomSpeed;
+		}
+
+		private void twoFingerTransformHandler(object sender, System.EventArgs e)
+		{
+			pivot.localPosition += pivot.rotation * TwoFingerMoveGesture.DeltaPosition * PanSpeed;
 		}
 		
 	}
