238b942ce6ff3e2cde9d8adf55f752d4eabbb491
Valentin Simonov <v@lent.in>
2015-11-15 10:53:43 +0300

    Added compile script for TUIO module.

---
+++ Modules/TUIO/Build/compile.sh
@@ -0,0 +1,17 @@
+printf "\n\e[32mCompiling TUIOSharp.\e[39m\n"
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+SLN="$DIR/../TUIOsharp/TUIOsharp.sln"
+
+# Mac
+if [ "$(uname)" == "Darwin" ]; then
+	/Applications/Unity/MonoDevelop.app/Contents/MacOS/mdtool build --t:Clean --c:Release $SLN 
+	/Applications/Unity/MonoDevelop.app/Contents/MacOS/mdtool build --t:Build --c:Release $SLN
+
+# Windows
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
+    /c/Windows/Microsoft.NET/Framework/v4.0.30319/msbuild.exe $SLN -p:Configuration=Release -clp:ErrorsOnly
+fi
+
+cp "$DIR/../TUIOsharp/TUIOsharp/bin/Release/TUIOsharp.dll" "$DIR/../Assets/TouchScript/Modules/TUIO/Libraries/"
+cp "$DIR/../TUIOsharp/TUIOsharp/bin/Release/OSCsharp.dll" "$DIR/../Assets/TouchScript/Modules/TUIO/Libraries/"
