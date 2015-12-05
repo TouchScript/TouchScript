ebf7326f6102268be67585917d2e2866098e9ae0
Valentin Simonov <v@lent.in>
2015-12-05 03:41:26 +0300

    Removed old build scripts.

--- Build/postbuild.bat
+++
@@ -1,8 +0,0 @@
-xcopy /y "%3" "%1UnityPackages\TouchScript\Assets\TouchScript\%2\"
-xcopy /y "%3" "%1UnityPackages\TouchScript.Android\Assets\TouchScript\%2\"
-xcopy /y "%3" "%1UnityPackages\TouchScript.iOS\Assets\TouchScript\%2\"
-xcopy /y "%3" "%1UnityPackages\TouchScript.TUIO\Assets\TouchScript\%2\"
-xcopy /y "%3" "%1UnityPackages\TouchScript.WIndowsPhone\Assets\TouchScript\%2\"
-xcopy /y "%3" "%1UnityPackages\TouchScript.WindowsStore\Assets\TouchScript\%2\"
-
-if exist custom_postbuild.bat custom_postbuild.bat "%1" "%2" "%3"
