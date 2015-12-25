aa3ac35cd793cb566ce35b673fcf529e5bea813d
Valentin Simonov <v@lent.in>
2015-12-25 22:21:39 +0600

    Cube example.

---
+++ Source/Assets/TouchScript/Examples/General/Cube/Scripts/LayerDelegate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using TouchScript.Layers;
+
+namespace TouchScript.Examples.Cube
+{
+    public class LayerDelegate : MonoBehaviour, ILayerDelegate
+    {
+
+        public RedirectInput Source;
+        public TouchLayer RenderTextureLayer;
+
+        public bool ShouldReceiveTouch(TouchLayer layer, TouchPoint touch)
+        {
+            if (layer == RenderTextureLayer)
+                return touch.InputSource == Source;
+            return touch.InputSource != Source;
+        }
+    }
+}
---
+++ Source/Assets/TouchScript/Examples/General/Cube/Scripts/RedirectInput.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using TouchScript.Gestures;
+using TouchScript.Hit;
+using TouchScript.InputSources;
+
+namespace TouchScript.Examples.Cube
+{
+    public class RedirectInput : InputSource
+    {
+
+        public int Width = 512;
+        public int Height = 512;
+
+        private MetaGesture gesture;
+        private Dictionary<int, int> map = new Dictionary<int, int>();
+
+        public override void CancelTouch(TouchPoint touch, bool @return)
+        {
+            base.CancelTouch(touch, @return);
+
+            map.Remove(touch.Id);
+            if (@return)
+            {
+                TouchHit hit;
+                if (!gesture.GetTargetHitResult(touch.Position, out hit)) return;
+                map.Add(touch.Id, beginTouch(processCoords(hit.RaycastHit.textureCoord), touch.Tags).Id);
+            }
+        }
+
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            gesture = GetComponent<MetaGesture>();
+            if (gesture)
+            {
+                gesture.TouchBegan += touchBeganHandler;
+                gesture.TouchMoved += touchMovedhandler;
+                gesture.TouchCancelled += touchCancelledhandler;
+                gesture.TouchEnded += touchEndedHandler;
+            }
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+
+            if (gesture)
+            {
+                gesture.TouchBegan -= touchBeganHandler;
+                gesture.TouchMoved -= touchMovedhandler;
+                gesture.TouchCancelled -= touchCancelledhandler;
+                gesture.TouchEnded -= touchEndedHandler;
+            }
+        }
+
+        private Vector2 processCoords(Vector2 value)
+        {
+            return new Vector2(value.x * Width, value.y * Height);
+        }
+
+        private void touchBeganHandler(object sender, MetaGestureEventArgs metaGestureEventArgs)
+        {
+            var touch = metaGestureEventArgs.Touch;
+            if (touch.InputSource == this) return;
+            map.Add(touch.Id, beginTouch(processCoords(touch.Hit.RaycastHit.textureCoord), touch.Tags).Id);
+        }
+
+        private void touchMovedhandler(object sender, MetaGestureEventArgs metaGestureEventArgs)
+        {
+            int id;
+            TouchHit hit;
+            var touch = metaGestureEventArgs.Touch;
+            if (touch.InputSource == this) return;
+            if (!map.TryGetValue(touch.Id, out id)) return;
+            if (!gesture.GetTargetHitResult(touch.Position, out hit)) return;
+            moveTouch(id, processCoords(hit.RaycastHit.textureCoord));
+        }
+
+        private void touchEndedHandler(object sender, MetaGestureEventArgs metaGestureEventArgs)
+        {
+            int id;
+            var touch = metaGestureEventArgs.Touch;
+            if (touch.InputSource == this) return;
+            if (!map.TryGetValue(touch.Id, out id)) return;
+            endTouch(id);
+        }
+
+        private void touchCancelledhandler(object sender, MetaGestureEventArgs metaGestureEventArgs)
+        {
+            int id;
+            var touch = metaGestureEventArgs.Touch;
+            if (touch.InputSource == this) return;
+            if (!map.TryGetValue(touch.Id, out id)) return;
+            cancelTouch(id);
+        }
+
+    }
+
+}
