9ffff058148a1b840c92be04aafbc237016eb5ba
Valentin Simonov <v@lent.in>
2016-08-09 22:29:48 +0300

    Fixed a few minor issues in examples list.

---
+++ Source/Assets/TouchScript/Examples/_misc/Scripts/ExamplesList.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class ExamplesList : MonoBehaviour 
+{
+
+	public RectTransform Content;
+
+	void Start () 
+	{
+		gameObject.SetActive(false);
+	}
+
+	public void ShowHide()
+	{
+		gameObject.SetActive(!gameObject.activeSelf);
+		Content.localPosition = Vector3.zero;
+	}
+
+}
---
+++ Source/Assets/TouchScript/Examples/_misc/Scripts/ShowMe.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShowMe : MonoBehaviour 
+{
+	IEnumerator Start () 
+	{
+		var canvas = GetComponent<Canvas>();
+		canvas.enabled = false;
+		yield return new WaitForSeconds(.5f);
+		canvas.enabled = true;
+	}
+}
