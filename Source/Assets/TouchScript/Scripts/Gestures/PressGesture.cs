bf651e3c289d39a00a484f08c21878f910c882ea
Valentin Simonov <v@lent.in>
2016-11-03 07:18:24 +0300

    Added some docs.

--- Source/Assets/TouchScript/Scripts/Gestures/PressGesture.cs
+++ Source/Assets/TouchScript/Scripts/Gestures/PressGesture.cs
@@ -12,9 +12,14 @@ using UnityEngine;
 namespace TouchScript.Gestures
 {
     /// <summary>
-    /// Recognizes when an object is touched.
-    /// Works with any gesture unless a Delegate is set.
+    /// Recognizes when an object is touched. Works with any gesture unless a Delegate is set.
     /// </summary>
+    /// <remarks>
+    /// <para>PressGesture fires immediately and would ultimately kill every other non-friendly gesture. So one would have to manually make it friendly with everything in a general use-case. That's why it's made friendly with everyone by default.</para>
+    /// <para>But there are cases when one would like to determine if parent container was pressed or its child. In current implementation both PressGestures will fire.</para>
+    /// <para>One approach would be to somehow make parent's PressGesture not friendly with child's one. But looking at how gesture recognition works we can see that this won't work. Since we would like child's gesture to fail parent's gesture. When child's PressGesture is recognized the system asks it if it can prevent parent's gesture, and it obviously can't because it's friendly with everything. And it doesn't matter that parent's gesture can be prevented by child's one... because child's one can't prevent parent's gesture and this is asked first.</para>
+    /// <para>This is basically what <see cref="IgnoreChildren"/> is for. It makes parent's PressGesture only listen for TouchPoints which lend directly on it.</para>
+    /// </remarks>
     [AddComponentMenu("TouchScript/Gestures/Press Gesture")]
     [HelpURL("http://touchscript.github.io/docs/html/T_TouchScript_Gestures_PressGesture.htm")]
     public class PressGesture : Gesture
--- Source/Assets/TouchScript/Scripts/Gestures/ReleaseGesture.cs
+++ Source/Assets/TouchScript/Scripts/Gestures/ReleaseGesture.cs
@@ -12,9 +12,9 @@ using UnityEngine;
 namespace TouchScript.Gestures
 {
     /// <summary>
-    /// Recognizes when last pointer is released from target.
-    /// Works with any gesture unless a Delegate is set.
+    /// Recognizes when last pointer is released from target. Works with any gesture unless a Delegate is set. 
     /// </summary>
+    /// <seealso cref="PressGesture"/>
     [AddComponentMenu("TouchScript/Gestures/Release Gesture")]
     [HelpURL("http://touchscript.github.io/docs/html/T_TouchScript_Gestures_ReleaseGesture.htm")]
     public class ReleaseGesture : Gesture
