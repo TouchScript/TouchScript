9d83418bd2ad6a96d37a036e4ed8df8adf52c26b
Valentin Simonov <v@lent.in>
2016-11-03 04:44:24 +0300

    Examples scene asks to add all examples to Build Settings. Examples list closes when an example is chosen.

--- Source/Assets/TouchScript/Examples/_misc/Scripts/Runner.cs
+++ Source/Assets/TouchScript/Examples/_misc/Scripts/Runner.cs
@@ -6,6 +6,10 @@ using UnityEngine;
 using TouchScript.Layers;
 using System.Collections;
 
+#if UNITY_EDITOR
+using UnityEditor;
+using System;
+#endif
 
 #if UNITY_5_3_OR_NEWER
 using UnityEngine.SceneManagement;
@@ -52,7 +56,7 @@ namespace TouchScript.Examples
 #endif
 		}
 
-        private void Start()
+		private void Start()
         {
             if (instance == null)
             {
@@ -61,6 +65,26 @@ namespace TouchScript.Examples
             }
 
 			layer = GetComponent<TouchLayer>();
+
+#if UNITY_EDITOR
+			var guids = AssetDatabase.FindAssets("t:Scene", new string[]{"Assets/TouchScript/Examples"});
+			if (EditorBuildSettings.scenes.Length != guids.Length)
+			{
+				if (EditorUtility.DisplayDialog("Add Example Scenes to Build Settings?", 
+					"You are running Examples scene but example scenes are not added to Build Settings. Do you want to add them now?", "Yes", "No"))
+				{
+					var importers = Array.ConvertAll(guids, (string guid) => AssetImporter.GetAtPath(AssetDatabase.GUIDToAssetPath(guid)));
+					Array.Sort(importers, (AssetImporter a, AssetImporter b) => {
+						var i1 = string.IsNullOrEmpty(a.userData) ? 42 : Convert.ToInt32(a.userData);
+						var i2 = string.IsNullOrEmpty(b.userData) ? 42 : Convert.ToInt32(b.userData);
+						if (i1 == i2) return 0;
+						return i1 - i2;
+					});
+					EditorBuildSettings.scenes = Array.ConvertAll(importers, (AssetImporter i) => new EditorBuildSettingsScene(i.assetPath, true));
+					EditorUtility.DisplayDialog("Success", "Example scenes were added to Build Settings. Please restart Play Mode.", "OK");
+				}
+			}
+#endif
 
 #if UNITY_5_3_OR_NEWER
             if (SceneManager.GetActiveScene().name == "Examples" && SceneManager.sceneCountInBuildSettings > 1)
