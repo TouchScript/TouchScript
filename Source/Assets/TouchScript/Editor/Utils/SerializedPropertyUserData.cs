364fdd21af29df1b28d45cd766e1e23be141e6a8
Valentin Simonov <v@lent.in>
2017-12-13 12:37:16 +0300

    Simplified SerializedPropertyUserData.

--- Source/Assets/TouchScript/Editor/Utils/SerializedPropertyUserData.cs
+++ Source/Assets/TouchScript/Editor/Utils/SerializedPropertyUserData.cs
@@ -1,45 +1,30 @@
 using System.Collections.Generic;
+using System.Text;
 using UnityEditor;
-using UnityEngine;
 
 namespace TouchScript.Editor.Utils
 {
     internal sealed class SerializedPropertyUserData<TUserData> where TUserData : class
     {
-        const int GCThreshold = 1000;
+        public static SerializedPropertyUserData<TUserData> Instance { get { return instance; } }
 
-        static SerializedPropertyUserData<TUserData> s_instance = new SerializedPropertyUserData<TUserData>();
-
-        public static SerializedPropertyUserData<TUserData> Instance { get { return s_instance; } }
-
-        private struct Storage
-        {
-            internal Object targetObject;
-            internal TUserData userData;
-        }
-
-        private Dictionary<string, Storage> storage = new Dictionary<string, Storage>();
-        private int accessCount = 0;
+        private static SerializedPropertyUserData<TUserData> instance = new SerializedPropertyUserData<TUserData>();
+        private static StringBuilder sb = new StringBuilder();
+        private Dictionary<string, TUserData> storage = new Dictionary<string, TUserData>();
 
         internal TUserData this[SerializedProperty property]
         {
             get
             {
-                GC();
                 var key = MakeKey(property);
-                Storage s;
-                if (!storage.TryGetValue(key, out s))
-                    return null;
-                return s.userData;
+                TUserData data;
+                if (!storage.TryGetValue(key, out data)) return null;
+                return data;
             }
             set
             {
                 var key = MakeKey(property);
-                storage[key] = new Storage
-                {
-                    targetObject = property.serializedObject.targetObject,
-                    userData = value,
-                };
+                storage[key] = value;
             }
         }
 
@@ -53,23 +38,10 @@ namespace TouchScript.Editor.Utils
             var to = so.targetObject;
             if (to == null)
                 throw new System.ArgumentException();
-            return to.GetInstanceID().ToString() + "," + property.propertyPath;
-        }
-
-        private void GC()
-        {
-            ++ accessCount;
-            if (accessCount < GCThreshold)
-                return;
-            accessCount = 0;
-            var newStorage = new Dictionary<string, Storage>();
-            foreach (var kv in storage)
-            {
-                if (kv.Value.targetObject != null)
-                    newStorage[kv.Key] = kv.Value;
-                //else Debug.LogFormat("GC: Expired: {0}", kv.Key);
-            }
-            storage = newStorage;
+            sb.Length = 0;
+            sb.Append(to.GetInstanceID().ToString());
+            sb.Append(property.propertyPath);
+            return sb.ToString();
         }
     }
 }
